8e2018d599094079ecb1fdb0f939579d9c84c2dc
shashi bhusan patel <bhusanshashi.2012@gmail.com>
2022-12-22 15:09:56 +0530

    1st commit

---
+++ src/main/java/com/jenkins/contoller/MainController.java
@@ -0,0 +1,14 @@
+package com.jenkins.contoller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("jenkins")
+public class MainController {
+	
+	public String helloJenkins() {
+		return "This is jenkins tutorials";
+	}
+
+}
---
+++ src/test/java/com/jenkins/contoller/MainControllerTest.java
@@ -0,0 +1,17 @@
+package com.jenkins.contoller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class MainControllerTest {
+
+	@Test
+	void test() {
+		assert(true);
+	}
+
+}
