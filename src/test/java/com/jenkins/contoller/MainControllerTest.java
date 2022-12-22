dcb003ee5bffb8b2ad34bf64a3c1ff8758647f42
shashi bhusan patel <bhusanshashi.2012@gmail.com>
2022-12-22 19:24:30 +0530

    add junit dependency

--- src/test/java/com/jenkins/contoller/MainControllerTest.java
+++ src/test/java/com/jenkins/contoller/MainControllerTest.java
@@ -1,6 +1,6 @@
 package com.jenkins.contoller;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 class MainControllerTest {
 
 	@Test
-	void test() {
+	public void test() {
 		assert(true);
 	}
 
