79e733e6e4ed82cae61ba9e8d7f531cfe74b4073
shashi bhusan patel <bhusanshashi.2012@gmail.com>
2022-12-22 17:57:21 +0530

    3rd commit

--- src/main/java/com/jenkins/JenkinsDemoApplication.java
+++ src/main/java/com/jenkins/JenkinsDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("===============test================");
+		System.out.println("============11===test================");
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
 
